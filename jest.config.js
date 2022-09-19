7ac93f58c185e3a479683e43cc231882a00f1d26
SoymichelDev <mtrana@ggtech.es>
2022-09-19 08:28:45 -0600

    feat: config typescript, prettier, eslint & jest

---
+++ jest.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  collectCoverageFrom: ['<rootDir>/src/***/*.ts'],
+  coverageDirectory: 'coverage',
+  coverageProvider: 'v8',
+  preset: 'ts-jest',
+  globals: {
+    "ts-jest": {
+      tsconfig: "tsconfig.json",
+      isolatedModules: true
+    },
+  },
+  moduleFileExtensions: [ "ts", "js" ],
+  roots: [ '<rootDir>/src', '<rootDir>/tests' ],
+  testEnvironment: 'node',
+  testMatch: [ '**/__tests__/**/*.+(ts)', '**/?(*.)+(spec|test).+(ts)' ],
+};

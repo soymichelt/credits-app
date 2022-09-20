8f329efae1967e8bc3c8b3830c96dee0b886b18b
SoymichelDev <mtrana@ggtech.es>
2022-09-19 23:56:24 -0600

    feat: configuring jest module mapper

--- jest.config.js
+++ jest.config.js
@@ -10,6 +10,9 @@ module.exports = {
     },
   },
   moduleFileExtensions: [ "ts", "js" ],
+  moduleNameMapper: {
+    '@/(.*)$': ['<rootDir>/src/$1']
+  },
   roots: [ '<rootDir>/src', '<rootDir>/tests' ],
   testEnvironment: 'node',
   testMatch: [ '**/__tests__/**/*.+(ts)', '**/?(*.)+(spec|test).+(ts)' ],
